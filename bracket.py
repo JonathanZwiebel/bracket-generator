7b59068f5a55fadd164d945bda2d809a63b09d45
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-01-03 22:07:08 -0800

    [JZ010218-1] Passed bracket file through linter

--- bracket.py
+++ bracket.py
@@ -2,11 +2,12 @@ import math
 import random
 import tkinter
 
+
 def generate_bracket_rec(players):
     games_map = {}
     next_players = []
 
-    assert len(players) > 0 
+    assert len(players) > 0
 
     if len(players) == 1:
         return games_map
@@ -16,16 +17,17 @@ def generate_bracket_rec(players):
         player2_id = len(players) - 1 - i
 
         if players[player2_id] is None:
-            next_players.append(players[player1_id])        
+            next_players.append(players[player1_id])
         else:
             game_id = str(int(math.log(len(players), 2))) + "g" + str(i + 1)
             g = Game((players[player1_id], players[player2_id]), game_id)
-            games_map[g.id] = g     
+            games_map[g.id] = g
             next_players.append((g, "winner"))
     d = {}
     d.update(games_map)
     d.update(generate_bracket_rec(next_players).items())
     return d
+
 
 # Players in each game will either be string literals
 # or tuples that contain a Game and "loser" or "winner"
@@ -37,8 +39,9 @@ class Game:
 
     def __str__(self):
         output = self.id + ": "
-        output = output + (self.players[0][0].get(self.players[0][1]) if type(self.players[0]) is tuple else self.players[0]) + " vs "
-        output = output + (self.players[1][0].get(self.players[1][1]) if type(self.players[1]) is tuple else self.players[1])
+        first_raw = self.players[0]
+        output += (first_raw[0].get(first_raw[1]) if type(self.players[0]) is tuple else first_raw) + " vs "
+        output += (self.players[1][0].get(self.players[1][1]) if type(self.players[1]) is tuple else self.players[1])
         return output
 
     def __repr__(self):
@@ -51,20 +54,18 @@ class Game:
 
     def is_ready(self):
         for player in self.players:
-            if type(player) is tuple and player[0].done == False:
+            if type(player) is tuple and not player[0].done:
                 return False
         return True
 
     def play_game(self, winning_player):
         # TODO: Deal with fact that this is just the is_ready method again
         for player in self.players:
-            if type(player) is tuple and player[0].done == False:
+            if type(player) is tuple and not player[0].done:
                 print("ERROR 1: Attempting to start " + self.id + " before " + player[0].id + " is complete")
                 return False
 
-
         players_raw = tuple(player[0].get(player[1]) if type(player) is tuple else player for player in self.players)
-
         if winning_player not in players_raw:
             print("ERROR 2: " + winning_player + " not in " + self.id)
             return False
@@ -85,7 +86,8 @@ class Game:
             else:
                 return "INVALID"
 
-# TODO: Make a more a general get function that checks if the type is a Tuple or a game and then evaluates
+# TODO: Make a more a general get function that checks if
+# the type is a Tuple or a game and then evaluates
 
 
 class Tournament:
@@ -93,15 +95,17 @@ class Tournament:
         self.players = players
         self.player_count = len(players)
         self.games = {}
+
     def __str__(self):
         output = ""
         for s in players:
-            output = output + ((s + " ") if s is not None else "" )
-        output = output + "\nlog(Size): " + str(self.n_size) + " | underflow: " + str(self.underflow)
+            output = output + ((s + " ") if s is not None else "")
+        output += "\nlog(Size): " + str(self.n_size)
+        output += " | underflow: " + str(self.underflow)
         return output
 
     def __repr__(self):
-        return __str__()
+        return self.__str__()
 
     def generate_bracket(self, bracket_class, randomize):
         if bracket_class != "single":
